b38236bd67ef04bfca0f3a771c79524b45108fc9
laurie60 <laurie8000@googlemail.com>
2022-08-11 20:14:26 +0100

    add API call within camera comonent

--- PocketForest/screens/CameraScreen.tsx
+++ PocketForest/screens/CameraScreen.tsx
@@ -1,41 +1,166 @@
-import { StyleSheet, Button } from 'react-native';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps, RootStackParamList } from '../types';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {useNavigation} from '@react-navigation/native';
+import { image } from "./cactus.json";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Image,
+} from "react-native";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Camera } from "expo-camera";
+import { shareAsync } from "expo-sharing";
+import * as MediaLibrary from "expo-media-library";
 
-type homeScreenProp = StackNavigationProp<RootStackParamList, 'Home'>;
+export default function App() {
+  console.log("data:image/png;base64," + image.substring(0, 100), "<<<<IMAGE");
+  let cameraRef = useRef();
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  const [photo, setPhoto] = useState();
+  const [plantData, setPlantData] = useState();
 
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === "granted");
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
+    })();
+  }, []);
 
-export default function CameraPage() {
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>;
+  } else if (!hasCameraPermission) {
+    return (
+      <Text>
+        Permission for camera not granted. Please change this in settings.
+      </Text>
+    );
+  }
 
+  let takePic = async () => {
+    let options = {
+      quality: 1,
+      base64: true,
+      exif: false,
+    };
 
-  const navigation = useNavigation<homeScreenProp>();
+    let newPhoto = await cameraRef.current.takePictureAsync(options);
+    setPhoto(newPhoto);
+  };
 
+  if (photo) {
+    const uploadPic = () => {
+      Promise.resolve(photo.base64).then((base64files) => {
+        //console.log(base64files);
+        const data = {
+          api_key: "K8TIo4HNKoGcvlcNkwI8dLGe2rUPNrYZhNgSuWYuym6AM3NBY9",
+          images: [`image/jpeg;base64,${base64files}`],
+          // modifiers docs: https://github.com/flowerchecker/Plant-id-API/wiki/Modifiers
+          modifiers: ["crops_fast", "similar_images"],
+          plant_language: "en",
+          // plant details docs: https://github.com/flowerchecker/Plant-id-API/wiki/Plant-details
+          plant_details: [
+            "common_names",
+            "url",
+            "name_authority",
+            "wiki_description",
+            "taxonomy",
+            "synonyms",
+          ],
+        };
+        fetch("https://api.plant.id/v2/identify", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => {
+            // console.log(response, "<<< response");
+            return response.json();
+          })
+          .then((data) => {
+            setPlantData(data);
+            // console.log("Success:", data);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      });
+    };
+
+    const sharePic = () => {
+      shareAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    const savePhoto = () => {
+      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+        setPhoto(undefined);
+      });
+    };
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Image
+          style={styles.preview}
+          source={{ uri: "data:image/jpg;base64," + photo.base64 }}
+        />
+        <Button title="upload" onPress={uploadPic} />
+
+        <Button title="Share" onPress={sharePic} />
+        {hasMediaLibraryPermission ? (
+          <Button title="Save" onPress={savePhoto} />
+        ) : undefined}
+        <Button title="Discard" onPress={() => setPhoto(undefined)} />
+      </SafeAreaView>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Camera Page will go here</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Button title="Forest" onPress={() => navigation.navigate('Forest')} />
-      <Text>When we take the picture, we will navigate to the Forest</Text>
-    </View>
+    <Camera style={styles.container} ref={cameraRef}>
+      <View style={styles.buttonContainer}>
+        <Button title="Take Pic" onPress={takePic} />
+      </View>
+      <StatusBar style="auto" />
+    </Camera>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    backgroundColor: "#fff",
+    alignSelf: "flex-end",
+  },
+  preview: {
+    alignSelf: "stretch",
+    flex: 1,
+  },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });

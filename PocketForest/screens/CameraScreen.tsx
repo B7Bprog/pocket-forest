9117aa60f345cfa7de1ff1fe452388ccf0144d10
laurie60 <laurie8000@googlemail.com>
2022-08-12 09:34:48 +0100

    add dependencies

--- PocketForest/screens/CameraScreen.tsx
+++ PocketForest/screens/CameraScreen.tsx
@@ -1,4 +1,3 @@
-import { image } from "./cactus.json";
 import { StatusBar } from "expo-status-bar";
 import {
   StyleSheet,
@@ -145,22 +144,5 @@ const styles = StyleSheet.create({
   preview: {
     alignSelf: "stretch",
     flex: 1,
-  },
-});
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
